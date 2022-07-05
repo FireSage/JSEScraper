a275015b2fd23a5da534b4c445c5ea5fe490b32b
Marvin Gordon <1299703+FireSage@users.noreply.github.com>
2022-07-05 02:50:34 -0400

    Load stock list working

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main 
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+var stockList string
+
+func loadJSE( url string ) string{
+	// initialise collector
+	c :=colly.NewCollector()
+	dataCollector := c.Clone()
+
+	// search for tables in html
+	c.OnHTML("table", func(e *colly.HTMLElement) {
+		if e.Index == 1 {
+			// fmt.Println(e.Index)
+
+			e.ForEach("tr", func(_ int, el *colly.HTMLElement){
+				//fmt.Println(el.ChildText("td:nth-child(2)"))
+				link := el.ChildAttr("td:nth-child(2) a ", "href")
+				//fmt.Println(link)
+				dataCollector.Visit(link)
+			})
+		}
+		
+
+	})
+	
+	dataCollector.OnHTML("h2", func(e *colly.HTMLElement){
+		//fmt.Println(e.Text)
+		stockList += ("(" + e.Text)
+		//fmt.Println("found")
+		//fmt.Println(e.Request.URL)
+	})
+
+	dataCollector.OnHTML(".tw-bg-gray-50.tw-rounded-sm:first-child", func(e *colly.HTMLElement){
+		//fmt.Println(e.Text)
+		fmt.Println(e.ChildText(".tw-flex.tw-justify-between.tw-bg-gray-50.tw-p-4:nth-child(5)"))
+		
+		stockList += ","
+		stockList += e.ChildText(".tw-flex.tw-justify-between.tw-bg-gray-50.tw-p-4:nth-child(5)")
+		stockList += ")\n"
+
+		fmt.Println(stockList)
+	})
+	//attempt to visit url
+	c.Visit(url)
+	
+	return stockList
+}
+
+func SayHello() string {
+	return "Hello, World"
+}
 
 func main(){
-	fmt.Println("Running")
+	// loadJSE("https://www.jamstockex.com/trading/trade-quotes/")
+
+	fmt.Println(loadJSE("https://www.jamstockex.com/trading/trade-quotes/"))
 }
