33a13c972267fe9dbaa0220473331da0a71ab7e1
Marvin Gordon <1299703+FireSage@users.noreply.github.com>
2023-05-27 16:15:28 -0500

    String processing for stocks; Stock struct added

--- main.go
+++ main.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"fmt"
-
+	"strings"
+	"strconv"
 	"github.com/gocolly/colly"
 )
 
-var stockList string
+// type Stock struct{
+// 	Name string
+// 	Ticker string
+// 	ShareCount uint
+// } 
+// func newStock(string name, string ticker uint sharecount) *Stock{
+// 	stock := Stock{
+// 		Name: name
+// 		Ticker: ticker
+// 		ShareCount: shareCount
+// 	}
+// 	return &stock
+// }
+
 
 func loadJSE( url string ) string{
+	var stockList string
 	// initialise collector
 	c :=colly.NewCollector()
 	dataCollector := c.Clone()
+	// stock := Stock
 
-	// search for tables in html
+	// search for table with main market stocks in html
 	c.OnHTML("table", func(e *colly.HTMLElement) {
-		if e.Index == 1 {
-			// fmt.Println(e.Index)
+		if e.Index == 2 {
 
 			e.ForEach("tr", func(_ int, el *colly.HTMLElement){
 				//fmt.Println(el.ChildText("td:nth-child(2)"))
@@ -26,26 +41,38 @@ func loadJSE( url string ) string{
 			})
 		}
 		
-
 	})
 	
 	dataCollector.OnHTML("h2", func(e *colly.HTMLElement){
-		//fmt.Println(e.Text)
-		stockList += ("(" + e.Text)
+		//fmt.Println(link)
+		nameStr := e.Text
+		name := (nameStr[0:strings.LastIndex(nameStr, " (")])
+		ticker := (nameStr[(strings.LastIndex(nameStr, " (")+2): strings.LastIndex(nameStr, ")")])
+		stockList += ("(" + name)
+		stockList += ("," + ticker)
 		//fmt.Println("found")
 		//fmt.Println(e.Request.URL)
+		fmt.Println(stockList);
 	})
 
 	dataCollector.OnHTML(".tw-bg-gray-50.tw-rounded-sm:first-child", func(e *colly.HTMLElement){
 		//fmt.Println(e.Text)
-		fmt.Println(e.ChildText(".tw-flex.tw-justify-between.tw-bg-gray-50.tw-p-4:nth-child(5)"))
+		// fmt.Println(e.ChildText(".tw-flex.tw-justify-between.tw-bg-gray-50.tw-p-4:nth-child(5)"))
 		
-		stockList += ","
-		stockList += e.ChildText(".tw-flex.tw-justify-between.tw-bg-gray-50.tw-p-4:nth-child(5)")
-		stockList += ")\n"
+		// fmt.Println(nameStr);
 
-		fmt.Println(stockList)
+		stockList += ", "
+		shareCountString := e.ChildText(".tw-flex.tw-justify-between.tw-bg-gray-50.tw-p-4:nth-child(5) span:nth-child(2)")
+		shareCount, err := strconv.ParseInt( strings.ReplaceAll(shareCountString[0: strings.LastIndex(shareCountString, "\n u")], ",", ""), 10, 64 )
+		if err == nil{
+			fmt.Println(shareCount)
+		}else{
+			fmt.Println(err)
+		}
+		// stockList += ")\n"
+
 	})
+
 	//attempt to visit url
 	c.Visit(url)
 	
@@ -57,7 +84,7 @@ func SayHello() string {
 }
 
 func main(){
-	// loadJSE("https://www.jamstockex.com/trading/trade-quotes/")
+	loadJSE("https://www.jamstockex.com/trading/trade-quotes/")
 
-	fmt.Println(loadJSE("https://www.jamstockex.com/trading/trade-quotes/"))
+	// fmt.Println(loadJSE("https://www.jamstockex.com/trading/trade-quotes/"))
 }
