e51ae9259f2d3b5125ceedeaa5e5f17f237efd2b
Marvin Gordon <1299703+FireSage@users.noreply.github.com>
2023-06-24 13:16:05 -0500

    Money values changed to be of type int

--- main.go
+++ main.go
@@ -9,34 +9,36 @@ import (
 )
 
 type Stock struct {
-	Name            string
-	Ticker          string
-	ShareCount      uint64
-	ClosingPrice    float32
-	LastTradedPrice float32
-	JSEUrl          string
+	Name             string
+	Ticker           string
+	ShareCount       uint64
+	ClosingPrice     int64
+	LastTradedPrice  int64
+	LastTradedVolume uint64
+	JSEUrl           string
 }
 
-func newStock(name string, ticker string, sharecount uint64, closingPrice float32, lastTradedPrice float32) *Stock {
+func newStock(name string, ticker string, sharecount uint64, closingPrice int64, lastTradedPrice int64, lastTradedVolume uint64) *Stock {
 	stock := Stock{
-		Name:            name,
-		Ticker:          ticker,
-		ShareCount:      sharecount,
-		ClosingPrice:    closingPrice,
-		LastTradedPrice: lastTradedPrice,
+		Name:             name,
+		Ticker:           ticker,
+		ShareCount:       sharecount,
+		ClosingPrice:     closingPrice,
+		LastTradedPrice:  lastTradedPrice,
+		LastTradedVolume: lastTradedVolume,
 	}
 	return &stock
 }
 
 func GetStocksWithUrls(market string) []string {
 	url := "https://www.jamstockex.com/trading/trade-quotes/?market=" + market + "-market"
-	var stockUrls []string
+	stockUrls := []string{}
 	c := colly.NewCollector()
 
 	// search for table with main market stocks in html
 	c.OnHTML("table", func(e *colly.HTMLElement) {
 		if e.Index == 2 {
-			e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			e.ForEach("tbody tr", func(_ int, el *colly.HTMLElement) {
 				stockUrls = append(stockUrls, el.ChildAttr("td:nth-child(2) a ", "href"))
 			})
 		}
@@ -52,10 +54,13 @@ func loadStock(url string) *Stock {
 	var name string
 	var ticker string
 	var shareCount uint64
-	var lastTradedPrice float32
-	var closingPrice float32
+	var lastTradedVolume uint64
+	var lastTradedPrice int64
+	var closingPrice int64
+
 	dataCollector := colly.NewCollector()
 
+	// get name, ticker, close price
 	dataCollector.OnHTML("h2", func(e *colly.HTMLElement) {
 		//fmt.Println(link)
 		nameStr := e.Text
@@ -63,38 +68,51 @@ func loadStock(url string) *Stock {
 		ticker = (nameStr[(strings.LastIndex(nameStr, " (") + 2):strings.LastIndex(nameStr, ")")])
 
 		priceStr := e.DOM.Next().Text()
-		closingPrice, err := strconv.ParseFloat(priceStr[strings.Index(priceStr, "$")+1:(strings.Index(priceStr, ".")+2)], 64)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(closingPrice)
-		}
+		priceStr = priceStr[strings.Index(priceStr, "$")+1 : (strings.Index(priceStr, ".") + 3)]
+		closingPrice = getDollarValueAsInt(priceStr) //, err := strconv.ParseFloat(priceStr[strings.Index(priceStr, "$")+1:(strings.Index(priceStr, ".")+3)], 64)
+		// if err != nil {
+		// fmt.Println(err)
+		// } else {
+		fmt.Println(closingPrice)
+		// }
 	})
 
+	// get share count
 	dataCollector.OnHTML(".tw-bg-gray-50.tw-rounded-sm:first-child", func(e *colly.HTMLElement) {
-
+		var err error
 		shareCountString := e.ChildText(".tw-flex.tw-justify-between.tw-bg-gray-50.tw-p-4:nth-child(5) span:nth-child(2)")
-		shareCount, err := strconv.ParseInt(strings.ReplaceAll(shareCountString[0:strings.LastIndex(shareCountString, "\n u")], ",", ""), 10, 64)
+		shareCount, err = strconv.ParseUint(strings.ReplaceAll(shareCountString[0:strings.LastIndex(shareCountString, "\n u")], ",", ""), 10, 64)
 		if err != nil {
-			fmt.Println(shareCount)
-		} else {
 			fmt.Println(err)
+		} else {
+			fmt.Println(shareCount)
 		}
 		// stockList += ")\n"
 
 	})
 
+	dataCollector.OnHTML(".tw-container > div:nth-child(2) .tw-justify-between span:nth-child(2)", func(e *colly.HTMLElement) {
+		switch e.Index {
+		case 4:
+			lastTradedVolumeString := e.Text
+			lastTradedVolume, _ = strconv.ParseUint(strings.ReplaceAll(lastTradedVolumeString, ",", ""), 10, 64)
+		case 5:
+			lastTradedPrice = getDollarValueAsInt(e.Text) //strconv.ParseFloat(e.Text[strings.Index(e.Text, "$")+1:(strings.Index(e.Text, ".")+3)], 64)
+			// TODO:: continue here
+
+		}
+	})
+
 	//attempt to visit url
 	dataCollector.Visit(url)
-
-	return newStock(name, ticker, shareCount, closingPrice, lastTradedPrice)
+	// TODO:: Throw error or retry when stock fails to load, fix
+	return newStock(name, ticker, shareCount, closingPrice, lastTradedPrice, lastTradedVolume)
 }
 func loadJSE() []Stock {
-	var stockList []Stock
+	stockList := make([]Stock, 0, 120)
 	urls := GetStocksWithUrls("main")
 
 	for _, url := range urls {
-		// TODO:: prevent empty first element
 		stock := loadStock(url)
 		stockList = append(stockList, *stock)
 	}
@@ -102,8 +120,17 @@ func loadJSE() []Stock {
 	return stockList
 }
 
-func SayHello() string {
-	return "Hello, World"
+func getDollarValueAsInt(moneyString string) int64 {
+	moneyString = strings.TrimSpace(moneyString)
+	moneyString = strings.Trim(moneyString, "$")
+	if strings.Index(moneyString, ".") == -1 {
+		moneyString = moneyString + "00"
+	} else {
+		moneyString = moneyString[:strings.Index(moneyString, ".")] + moneyString[strings.Index(moneyString, ".")+1:strings.Index(moneyString, ".")+3]
+	}
+	moneyInt, _ := strconv.ParseInt(moneyString, 10, 64)
+
+	return moneyInt
 }
 
 func main() {
